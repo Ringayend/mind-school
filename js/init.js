3d61fbd4bf94f9ab2c75a53d6b25381a88ec8cd8
RINGAYEN David <davidringayen@MacBook-Pro-de-David.local>
2016-05-06 14:34:14 +0200

    page statique general

--- js/init.js
+++ js/init.js
@@ -6,3 +6,25 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+    $('select').material_select();
+  });
+ 
+ $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 80, // Creates a dropdown of 15 years to control year
+    format: 'yyyy/mm/dd',
+    formatSubmit: 'yyyy/mm/dd',
+    max: true
+
+  });
+
+  $(document).ready(function() {
+    $('select').material_select();
+  });
+
+   $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
